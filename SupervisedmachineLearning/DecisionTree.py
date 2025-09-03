9e97af9a72d78e0e20b1cd073dc684ad1bddd956
shashank123456755776 <114920880+shashank123456755776@users.noreply.github.com>
2025-09-03 17:29:35 +0530

    commit4

---
+++ SupervisedmachineLearning/DecisionTree.py
@@ -0,0 +1,17 @@
+from sklearn.tree import DecisionTreeClassifier 
+X=  [[7,2],#apple
+    [8,3],#apple
+    [9,8],#orange
+    [18,9],#orange
+]
+y=[0,0,1,1]
+model = DecisionTreeClassifier()
+model.fit(X,y)
+size=float(input("enter size in cm:"))
+shade=float(input("enter color shade (1-10):"))
+result=model.predict([[size,shade]])[0]
+if result ==0:
+    print("This is likely to be Apple")
+else:
+    print("This is likely to be Orange")
+        
