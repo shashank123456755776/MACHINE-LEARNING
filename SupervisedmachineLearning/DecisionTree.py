3b943ba7f6c2d551a3c1fb46de415b553541d632
shashank123456755776 <114920880+shashank123456755776@users.noreply.github.com>
2025-09-04 12:57:44 +0530

    commit5

--- SupervisedmachineLearning/DecisionTree.py
+++ SupervisedmachineLearning/DecisionTree.py
@@ -15,3 +15,6 @@ if result ==0:
 else:
     print("This is likely to be Orange")
         
+#Underfitting --deeply focus on given example if yoy give another exapmples to predict it fails
+#Overfititng--menas he will consider evrything same like cat exapmle
+#Goodfitting-- hmm models ko particular chijj baatate hai       
