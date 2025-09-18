8e2fcb30f41d0dd87e1fd6443fe9888360e1e140
shashank123456755776 <114920880+shashank123456755776@users.noreply.github.com>
2025-09-18 16:07:59 +0530

    commit20

--- Unsupervisedmachineleraning/EffectiveDataAnalysis/projects1.py
+++ Unsupervisedmachineleraning/EffectiveDataAnalysis/projects1.py
@@ -11,20 +11,23 @@ df=pd.read_csv("studentsuccess.csv")
 le=LabelEncoder()
 df["Internet"]=le.fit_transform(df["Internet"])
 df["Passed"]=le.fit_transform(df["Passed"])
+print(df)
 features=["StudyHours","Attendance","PastScore","Sleephours"]
 scaler=StandardScaler()
 df_scaled=df.copy()
 df_scaled[features]=scaler.fit_transform(df[features])
 X=df_scaled[features] #features
+print(X)
 y=df_scaled["Passed"] #target 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42) 
 model =LogisticRegression()
 model.fit(X_train,y_train)
-y_predict=model.predict(X_test)
+y_predict=model.predict(X_test) #testing leads to predicting
 # classification report
 print("Classification Report:")
-print(classification_report(y_test,y_predict))
+print(classification_report(y_test,y_predict)) #actual output testing ka versus predicted output testing ka
 # confusing matrix
+# print(classification_report(y_test, y_predict)) ka kaam hai → model ke performance ka summary nikalna alag-alag metrics ke saath.
 
 confusion_matrix= confusion_matrix(y_test,y_predict)
 plt.figure(figsize=(6,4))
@@ -53,3 +56,14 @@ try:
     print(f"prediction Based on input:{result}")
 except Exception as e:
     print("an error occured" ,e)    
+    
+    # notes
+#     precision → Jab model ne bola “Yes” (or “No”), to kitni baar wo sahi tha?
+# Formula = TP / (TP + FP)
+
+# recall → Kitne actual “Yes” (or “No”) ko model ne correctly detect kiya?
+# Formula = TP / (TP + FN)
+
+# f1-score → Precision aur Recall ka harmonic mean (balance measure).
+
+# support → Test dataset me us class ke kitne samples the.
